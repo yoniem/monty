ec133038daa8ea5849b1ef0ae6aa5ac8d7750f0a
yoniem <yonimphini@gmail.com>
2023-08-24 13:21:45 -0700

    update

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	open_file(argv[1]);
+	free_nodes();
+
+	return (0);
+}
